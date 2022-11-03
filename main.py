379cfe0820acc830e6575f6261ead996f1117d57
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2022-11-03 22:20:58 +0200

    fix copy_file

--- main.py
+++ main.py
@@ -27,36 +27,34 @@ async def scan_folder(path: AsyncPath) -> None:
             await copy_file(el)
 
 
-async def copy_file(path: AsyncPath) -> None:
-    async for el in path.iterdir():
-        if el.is_file():
-            ext = el.suffix.casefold()
-            if ext in ['.jpg', '.png', '.jpeg', '.bmp', '.gif', 'svg']:
-                new_path = output_folder / 'images' / ext[1:].upper()
-            elif ext in ['.mp3', '.wav', '.ogg', '.flac', '.aac', '.wma', '.m4a']:
-                new_path = output_folder / 'audio' / ext[1:].upper()
-            elif ext in ['.avi', '.mpg', '.mpeg', '.mkv', '.mov', '.flv', '.wmv', '.mp4', '.webm', '.mp4']:
-                new_path = output_folder / 'video' / ext[1:].upper()
-            elif ext in ['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf', '.txt', '.rtf', '.csv']:
-                new_path = output_folder / 'documents' / ext[1:].upper()
-            elif ext in ['.zip', '.tag', '.gz']:
-                new_path = output_folder / 'archives' / ext[1:].upper()
-            else:
-                new_path = output_folder / 'other'
-            try:
-                await new_path.mkdir(parents=True, exist_ok=True)
-                await copyfile(el, new_path / normalize(el.name))
-            except OSError as err:
-                logging.error(f"Can't copy file {el.name}. Error: {err}")
+async def copy_file(file: AsyncPath) -> None:
+        ext = file.suffix.casefold()
+        if ext in ['.jpg', '.png', '.jpeg', '.bmp', '.gif', 'svg']:
+            new_path = output_folder / 'images' / ext[1:].upper()
+        elif ext in ['.mp3', '.wav', '.ogg', '.flac', '.aac', '.wma', '.m4a']:
+            new_path = output_folder / 'audio' / ext[1:].upper()
+        elif ext in ['.avi', '.mpg', '.mpeg', '.mkv', '.mov', '.flv', '.wmv', '.mp4', '.webm', '.mp4']:
+            new_path = output_folder / 'video' / ext[1:].upper()
+        elif ext in ['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf', '.txt', '.rtf', '.csv']:
+            new_path = output_folder / 'documents' / ext[1:].upper()
+        elif ext in ['.zip', '.tag', '.gz']:
+            new_path = output_folder / 'archives' / ext[1:].upper()
+        else:
+            new_path = output_folder / 'other'
+        try:
+            await new_path.mkdir(parents=True, exist_ok=True)
+            await copyfile(file, new_path / normalize(file.name))
+        except OSError as err:
+            print(f"Can't copy file {file.name}. Error: {err}")
 
 
 if __name__ == '__main__':
     start = time()
-    logging.basicConfig(level=logging.DEBUG, format="%(threadName)s %(message)s")
     asyncio.run(scan_folder(AsyncPath(source)))
     print(f'Sorting folder complete in {time() - start} seconds')
     print("Do you want to delete the source folder? (y/n)")
     if input() == "y":
         asyncio.run(rmtree(AsyncPath(source)))
         print("Source folder deleted successfully")
+    print('Goodbye!')
 
