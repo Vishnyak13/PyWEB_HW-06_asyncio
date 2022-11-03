4b3c41bb294ec29481e8c851c693d8e8ae2d61ca
Eugene Vyshnytsky <101946519+Vishnyak13@users.noreply.github.com>
2022-11-03 22:25:08 +0200

    fix PEP8

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from aioshutil import copyfile, rmtree
 import logging
 from normalize import normalize
 
-
 parser = argparse.ArgumentParser(description='Sorting folder')
 parser.add_argument("--source", "-s", help="Path to trash folder for scan", required=True)
 parser.add_argument("--output", "-o", help="Path to sorted folder by file extension", default="sort_dist")
@@ -28,24 +27,24 @@ async def scan_folder(path: AsyncPath) -> None:
 
 
 async def copy_file(file: AsyncPath) -> None:
-        ext = file.suffix.casefold()
-        if ext in ['.jpg', '.png', '.jpeg', '.bmp', '.gif', 'svg']:
-            new_path = output_folder / 'images' / ext[1:].upper()
-        elif ext in ['.mp3', '.wav', '.ogg', '.flac', '.aac', '.wma', '.m4a']:
-            new_path = output_folder / 'audio' / ext[1:].upper()
-        elif ext in ['.avi', '.mpg', '.mpeg', '.mkv', '.mov', '.flv', '.wmv', '.mp4', '.webm', '.mp4']:
-            new_path = output_folder / 'video' / ext[1:].upper()
-        elif ext in ['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf', '.txt', '.rtf', '.csv']:
-            new_path = output_folder / 'documents' / ext[1:].upper()
-        elif ext in ['.zip', '.tag', '.gz']:
-            new_path = output_folder / 'archives' / ext[1:].upper()
-        else:
-            new_path = output_folder / 'other'
-        try:
-            await new_path.mkdir(parents=True, exist_ok=True)
-            await copyfile(file, new_path / normalize(file.name))
-        except OSError as err:
-            print(f"Can't copy file {file.name}. Error: {err}")
+    ext = file.suffix.casefold()
+    if ext in ['.jpg', '.png', '.jpeg', '.bmp', '.gif', 'svg']:
+        new_path = output_folder / 'images' / ext[1:].upper()
+    elif ext in ['.mp3', '.wav', '.ogg', '.flac', '.aac', '.wma', '.m4a']:
+        new_path = output_folder / 'audio' / ext[1:].upper()
+    elif ext in ['.avi', '.mpg', '.mpeg', '.mkv', '.mov', '.flv', '.wmv', '.mp4', '.webm', '.mp4']:
+        new_path = output_folder / 'video' / ext[1:].upper()
+    elif ext in ['.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf', '.txt', '.rtf', '.csv']:
+        new_path = output_folder / 'documents' / ext[1:].upper()
+    elif ext in ['.zip', '.tag', '.gz']:
+        new_path = output_folder / 'archives' / ext[1:].upper()
+    else:
+        new_path = output_folder / 'other'
+    try:
+        await new_path.mkdir(parents=True, exist_ok=True)
+        await copyfile(file, new_path / normalize(file.name))
+    except OSError as err:
+        print(f"Can't copy file {file.name}. Error: {err}")
 
 
 if __name__ == '__main__':
@@ -57,4 +56,3 @@ if __name__ == '__main__':
         asyncio.run(rmtree(AsyncPath(source)))
         print("Source folder deleted successfully")
     print('Goodbye!')
-
